324cf233f7db8cd7953eeb5a20d2b3dfcc271a34
Elmurod2000 <elmurodburiboev@gmail.com>
2022-05-06 19:41:12 +0600

    Initial commit

---
+++ app/src/main/java/com/example/testapp41/Math.java
@@ -0,0 +1,57 @@
+package com.example.testapp41;
+
+
+import java.util.regex.Pattern;
+
+public class Math {
+    private Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
+    public String add(String a, String b) {
+        String result = "";
+        if (a.isEmpty() || b.isEmpty()) {
+            return "Пусто";
+        }
+        if (isNumeric(a) && isNumeric(b)) {
+            int num1 = 0;
+            try {
+                num1 = Integer.parseInt(a);
+            } catch (NumberFormatException e) {
+                double d = Double.parseDouble(a);
+                num1= (int) d;
+            }
+            int num2 = 0;
+            try {
+                num2 = Integer.parseInt(b);
+            } catch (NumberFormatException e) {
+                double d = Double.parseDouble(b);
+                num2= (int) d;
+            }
+
+            result = String.valueOf(num1 + num2);
+        } else {
+            result = "Буквы нельзя";
+            System.out.println("Буквы нельзя");
+        }
+        return result;
+    }
+
+    public String divide(String a, String b) {
+        String result = "";
+        if (b.equals("0") || a.equals("0")) {
+            result = "на ноль нельзя";
+        } else {
+            int num1 = Integer.parseInt(a);
+            int num2 = Integer.parseInt(b);
+            return String.valueOf(num1 / num2);
+        }
+        return result;
+    }
+
+
+    public boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return pattern.matcher(strNum).matches();
+    }
+}
